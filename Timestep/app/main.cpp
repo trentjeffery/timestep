5275695c6e2e0e00f4ff254748917d816d22da9a
Trent Jeffery <trentdjeffery@gmail.com>
2019-05-20 11:46:15 -0700

    Get SDL2 linking and the program running. Add main game loop.

--- Timestep/app/main.cpp
+++ Timestep/app/main.cpp
@@ -1,8 +1,34 @@
 
 #include <SDL.h>
+#include <iostream>
+
+void Init()
+{
+	std::cout << "Init\n";
+}
+
+bool Update()
+{
+	std::cout << "Update\n";
+	return true;
+}
+
+void Cleanup()
+{
+	std::cout << "Cleanup\n";
+}
 
 int main(int argc, char* argv[])
 {
 	// run game loop
+	Init();
+	
+	while (Update())
+	{
+
+	}
+	
+	Cleanup();
+
 	return 0;
 }
